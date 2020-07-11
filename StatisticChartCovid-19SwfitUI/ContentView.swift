a1a21a3ccd7b801cf8c70615472099ca3749a136
Danny-Vu <vudinhbao.nz@gmail.com>
2020-07-11 09:50:05 +0700

    Initial Commit

---
+++ StatisticChartCovid-19SwfitUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatisticChartCovid-19SwfitUI
+//
+//  Created by Bao Vu on 7/11/20.
+//  Copyright © 2020 Bao Vu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
